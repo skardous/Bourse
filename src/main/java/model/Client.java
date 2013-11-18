844977b3eb2077749964dc3147757087bb6a1757
thiergeo <geoffrey.thierry@gmail.com>
2013-11-18 15:29:24 +0100

    Création EJB Entité Client

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,8 +1,45 @@
 package model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-@Entity
+@Entity(name = "Client")
 public class Client {
-    //
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private String lastName;
+
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
