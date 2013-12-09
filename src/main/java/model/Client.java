d9dc7d0152b759638ba37d893a83fd93f9dcb751
simon <simon.kardous@gmail.com>
2013-12-09 13:07:21 +0100

    conflict

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,8 +1,49 @@
 package model;
 
-import javax.persistence.Entity;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.*;
 
-@Entity
+@Entity(name = "Client")
+@ManagedBean
+@SessionScoped
 public class Client {
-    //
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String lastName;
+    private String password;
+    @OneToOne
+    private Portefeuille portefeuille;
+    @OneToOne
+    private Compte compte;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
